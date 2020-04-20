14ead1cf25493333da219241b696c366343b8985
Tim Besard <tim.besard@gmail.com>
2020-04-20 09:20:53 +0200

    Make the command queue ctor configurable.

--- src/cmdqueue.jl
+++ src/cmdqueue.jl
@@ -5,17 +5,20 @@ export ZeCommandQueue, synchronize
 mutable struct ZeCommandQueue
     handle::ze_command_queue_handle_t
 
-    function ZeCommandQueue(dev::ZeDevice)
+    dev::ZeDevice
+    ordinal::Int
+
+    function ZeCommandQueue(dev::ZeDevice, ordinal=0;
+                            flags=ZE_COMMAND_QUEUE_FLAG_NONE,
+                            mode::ze_command_queue_mode_t=ZE_COMMAND_QUEUE_MODE_DEFAULT,
+                            priority::ze_command_queue_priority_t=ZE_COMMAND_QUEUE_PRIORITY_NORMAL)
         desc_ref = Ref(ze_command_queue_desc_t(
             ZE_COMMAND_QUEUE_DESC_VERSION_CURRENT,
-            ZE_COMMAND_QUEUE_FLAG_NONE,
-            ZE_COMMAND_QUEUE_MODE_DEFAULT,
-            ZE_COMMAND_QUEUE_PRIORITY_NORMAL,
-            0
+            flags, mode, priority, ordinal
         ))
         handle_ref = Ref{ze_command_queue_handle_t}()
         zeCommandQueueCreate(dev, desc_ref, handle_ref)
-        obj = new(handle_ref[])
+        obj = new(handle_ref[], dev, ordinal)
         finalizer(obj) do obj
             zeCommandQueueDestroy(obj)
         end
