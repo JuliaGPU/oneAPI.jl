7a643746c4bf66077853deb50117a06fddeedb54
Tim Besard <tim.besard@gmail.com>
2020-07-07 12:26:00 +0200

    Fix wrapper for zeDeviceMakeMemoryResident.

--- lib/level-zero/residency.jl
+++ lib/level-zero/residency.jl
@@ -4,7 +4,7 @@ export make_resident, evict
 ## memory
 
 make_resident(dev::ZeDevice, buf::Buffer, size=sizeof(buf)) =
-    zeDeviceEvictMemory(dev, buf, size)
+    zeDeviceMakeMemoryResident(dev, buf, size)
 
 evict(dev::ZeDevice, buf::Buffer, size=sizeof(buf)) =
     zeDeviceEvictMemory(dev, buf, size)
