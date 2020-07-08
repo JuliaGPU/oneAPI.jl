5f460c84d32d41ac5e91e1a1abe8b1a50bb2f974
Tim Besard <tim.besard@gmail.com>
2020-07-08 08:25:28 +0200

    Make memory copies synchronizing for now.

--- src/memory.jl
+++ src/memory.jl
@@ -5,6 +5,10 @@ function Base.unsafe_copyto!(dev::ZeDevice, dst::Union{Ptr{T},ZePtr{T}},
     execute!(global_queue(dev)) do list
         append_copy!(list, dst, src, N*sizeof(T))
     end
+
+    # memory copies are synchronizing
+    # TODO: this is costly; figure out a better programming model
+    synchronize(global_queue(device()))
 end
 
 function unsafe_fill!(dev::ZeDevice, ptr::Union{Ptr{T},ZePtr{T}},
