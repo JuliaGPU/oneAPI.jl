f497759249b34678c2492550cf60c488b89586f8
Tim Besard <tim.besard@gmail.com>
2020-08-21 15:38:43 +0200

    Clean up the Buffer interface.

--- lib/level-zero/memory.jl
+++ lib/level-zero/memory.jl
@@ -7,30 +7,18 @@ export device_alloc, host_alloc, shared_alloc, free, properties, lookup_alloc
 # untyped buffers
 #
 
-abstract type Buffer end
+abstract type AbstractBuffer end
 
-# expected interface:
-# - similar()
-# - ptr, bytesize and drv fields
-# - convert() to certain pointers
-
-Base.pointer(buf::Buffer) = buf.ptr
-
-Base.sizeof(buf::Buffer) = buf.bytesize
+Base.convert(T::Type{<:Union{Ptr,ZePtr}}, buf::AbstractBuffer) =
+    throw(ArgumentError("Illegal conversion of a $(typeof(buf)) to a $T"))
 
 # ccall integration
 #
 # taking the pointer of a buffer means returning the underlying pointer,
 # and not the pointer of the buffer object itself.
-Base.unsafe_convert(P::Type{<:Ptr},   buf::Buffer) = convert(P, buf)
-Base.unsafe_convert(P::Type{<:ZePtr}, buf::Buffer) = convert(P, buf)
+Base.unsafe_convert(P::Type{<:Union{Ptr,ZePtr}}, buf::AbstractBuffer) = convert(P, buf)
 
-free(buf::Buffer) = zeMemFree(buf.context, buf)
-
-function Base.show(io::IO, ::MIME"text/plain", buf::Buffer)
-    print(io, Base.format_bytes(sizeof(buf)), " ", nameof(typeof(buf)),
-          " at 0x", string(UInt(pointer(buf)), base=16))
-end
+free(buf::AbstractBuffer) = zeMemFree(context(buf), buf)
 
 
 ## device buffer
@@ -41,22 +29,11 @@ end
 A buffer of device memory, owned by a specific device. Generally, may only be accessed by
 the device that owns it.
 """
-struct DeviceBuffer <: Buffer
+struct DeviceBuffer <: AbstractBuffer
     ptr::ZePtr{Cvoid}
     bytesize::Int
     context::ZeContext
 end
-
-Base.similar(buf::DeviceBuffer, ptr::ZePtr{Cvoid}=pointer(buf),
-             bytesize::Int=sizeof(buf), drv::ZeDriver=buf.drv) =
-    DeviceBuffer(ptr, bytesize, ctx)
-
-Base.convert(::Type{<:Ptr}, buf::DeviceBuffer) =
-    throw(ArgumentError("cannot take the host address of a device buffer"))
-
-Base.convert(::Type{ZePtr{T}}, buf::DeviceBuffer) where {T} =
-    convert(ZePtr{T}, pointer(buf))
-
 
 function device_alloc(ctx::ZeContext, dev::ZeDevice, bytesize::Integer, alignment::Integer=1;
                       flags=0, ordinal::Integer=0)
@@ -71,6 +48,16 @@ function device_alloc(ctx::ZeContext, dev::ZeDevice, bytesize::Integer, alignmen
     return DeviceBuffer(reinterpret(ZePtr{Cvoid}, ptr_ref[]), bytesize, ctx)
 end
 
+Base.pointer(buf::DeviceBuffer) = buf.ptr
+Base.sizeof(buf::DeviceBuffer) = buf.bytesize
+context(buf::DeviceBuffer) = buf.context
+
+Base.show(io::IO, buf::DeviceBuffer) =
+    @printf(io, "DeviceBuffer(%s at %p)", Base.format_bytes(sizeof(buf)), Int(pointer(buf)))
+
+Base.convert(::Type{ZePtr{T}}, buf::DeviceBuffer) where {T} =
+    convert(ZePtr{T}, pointer(buf))
+
 
 ## host buffer
 
@@ -84,22 +71,11 @@ Note that these buffers need to be made resident to the device, e.g., by using t
 ZE_KERNEL_FLAG_FORCE_RESIDENCY module flag, the ZE_KERNEL_SET_ATTR_INDIRECT_HOST_ACCESS
 kernel attribute, or by calling zeDeviceMakeMemoryResident.
 """
-struct HostBuffer <: Buffer
+struct HostBuffer <: AbstractBuffer
     ptr::Ptr{Cvoid}
     bytesize::Int
     context::ZeContext
 end
-
-Base.similar(buf::HostBuffer, ptr::Ptr{Cvoid}=pointer(buf),
-             bytesize::Int=sizeof(buf), drv::ZeDriver=buf.drv) =
-    HostBuffer(ptr, bytesize, ctx, mapped)
-
-Base.convert(::Type{Ptr{T}}, buf::HostBuffer) where {T} =
-    convert(Ptr{T}, pointer(buf))
-
-Base.convert(::Type{ZePtr{T}}, buf::HostBuffer) where {T} =
-    reinterpret(ZePtr{T}, pointer(buf))
-
 
 function host_alloc(ctx::ZeContext, bytesize::Integer, alignment::Integer=1; flags=0)
     desc_ref = Ref(ze_host_mem_alloc_desc_t(
@@ -113,6 +89,19 @@ function host_alloc(ctx::ZeContext, bytesize::Integer, alignment::Integer=1; fla
     return HostBuffer(ptr_ref[], bytesize, ctx)
 end
 
+Base.pointer(buf::HostBuffer) = buf.ptr
+Base.sizeof(buf::HostBuffer) = buf.bytesize
+context(buf::HostBuffer) = buf.context
+
+Base.show(io::IO, buf::HostBuffer) =
+    @printf(io, "HostBuffer(%s at %p)", Base.format_bytes(sizeof(buf)), Int(pointer(buf)))
+
+Base.convert(::Type{Ptr{T}}, buf::HostBuffer) where {T} =
+    convert(Ptr{T}, pointer(buf))
+
+Base.convert(::Type{ZePtr{T}}, buf::HostBuffer) where {T} =
+    reinterpret(ZePtr{T}, pointer(buf))
+
 
 ## shared buffer
 
@@ -121,22 +110,11 @@ end
 
 A managed buffer that is shared between the host and one or more devices.
 """
-struct SharedBuffer <: Buffer
+struct SharedBuffer <: AbstractBuffer
     ptr::ZePtr{Cvoid}
     bytesize::Int
     context::ZeContext
 end
-
-Base.similar(buf::SharedBuffer, ptr::ZePtr{Cvoid}=pointer(buf),
-             bytesize::Int=sizeof(buf), dev::ZeDriver=buf.dev) =
-    SharedBuffer(ptr, bytesize, ctx)
-
-Base.convert(::Type{Ptr{T}}, buf::SharedBuffer) where {T} =
-    convert(Ptr{T}, reinterpret(Ptr{Cvoid}, pointer(buf)))
-
-Base.convert(::Type{ZePtr{T}}, buf::SharedBuffer) where {T} =
-    convert(ZePtr{T}, pointer(buf))
-
 
 function shared_alloc(ctx::ZeContext, dev::Union{Nothing,ZeDevice}, bytesize::Integer,
                       alignment::Integer=1; host_flags=0,
@@ -157,10 +135,23 @@ function shared_alloc(ctx::ZeContext, dev::Union{Nothing,ZeDevice}, bytesize::In
     return SharedBuffer(reinterpret(ZePtr{Cvoid}, ptr_ref[]), bytesize, ctx)
 end
 
+Base.pointer(buf::SharedBuffer) = buf.ptr
+Base.sizeof(buf::SharedBuffer) = buf.bytesize
+context(buf::SharedBuffer) = buf.context
+
+Base.show(io::IO, buf::SharedBuffer) =
+    @printf(io, "SharedBuffer(%s at %p)", Base.format_bytes(sizeof(buf)), Int(pointer(buf)))
+
+Base.convert(::Type{Ptr{T}}, buf::SharedBuffer) where {T} =
+    convert(Ptr{T}, reinterpret(Ptr{Cvoid}, pointer(buf)))
+
+Base.convert(::Type{ZePtr{T}}, buf::SharedBuffer) where {T} =
+    convert(ZePtr{T}, pointer(buf))
+
 
 ## properties
 
-function properties(buf::Buffer)
+function properties(buf::AbstractBuffer)
     props_ref = Ref{ze_memory_allocation_properties_t}()
     dev_ref = Ref{ze_device_handle_t}(C_NULL)
     zeMemGetAllocProperties(buf.context, pointer(buf), props_ref, dev_ref)
@@ -173,11 +164,18 @@ function properties(buf::Buffer)
     )
 end
 
-struct UnknownBuffer <: Buffer
+struct UnknownBuffer <: AbstractBuffer
     ptr::Ptr{Cvoid}
     bytesize::Int
     context::ZeContext
 end
+
+Base.pointer(buf::UnknownBuffer) = buf.ptr
+Base.sizeof(buf::UnknownBuffer) = buf.bytesize
+context(buf::UnknownBuffer) = buf.context
+
+Base.show(io::IO, buf::UnknownBuffer) =
+    @printf(io, "UnknownBuffer(%s at %p)", Base.format_bytes(sizeof(buf)), Int(pointer(buf)))
 
 function lookup_alloc(ctx::ZeContext, ptr::Union{Ptr,ZePtr})
     base_ref = Ref{Ptr{Cvoid}}()
--- lib/level-zero/residency.jl
+++ lib/level-zero/residency.jl
@@ -3,8 +3,8 @@ export make_resident, evict
 
 ## memory
 
-make_resident(ctx::ZeContext, dev::ZeDevice, buf::Buffer, size=sizeof(buf)) =
+make_resident(ctx::ZeContext, dev::ZeDevice, buf::AbstractBuffer, size=sizeof(buf)) =
     zeContextMakeMemoryResident(ctx, dev, buf, size)
 
-evict(ctx::ZeContext, dev::ZeDevice, buf::Buffer, size=sizeof(buf)) =
+evict(ctx::ZeContext, dev::ZeDevice, buf::AbstractBuffer, size=sizeof(buf)) =
     zeContextEvictMemory(ctx, dev, buf, size)
