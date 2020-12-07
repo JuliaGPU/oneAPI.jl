56285d0bc52d8594a1b353aebbb977d667adacc8
Tim Besard <tim@juliacomputing.com>
2020-12-07 08:35:42 +0100

    Add a quickfail worker option.

--- test/runtests.jl
+++ test/runtests.jl
@@ -31,12 +31,14 @@ if do_help
 
                --help             Show this text.
                --list             List all available tests.
+               --quickfail        Fail the entire run as soon as a single test errored.
                --jobs=N           Launch `N` processes to perform tests (default: Threads.nthreads()).
 
                Remaining arguments filter the tests that will be executed.""")
     exit(0)
 end
 _, jobs = extract_flag!(ARGS, "--jobs", Threads.nthreads())
+do_quickfail, _ = extract_flag!(ARGS, "--quickfail")
 
 include("setup.jl")     # make sure everything is precompiled
 
@@ -179,9 +181,9 @@ all_tests = copy(tests)
 try
     # Monitor stdin and kill this task on ^C
     # but don't do this on Windows, because it may deadlock in the kernel
+    t = current_task()
     running_tests = Dict{String, DateTime}()
     if !Sys.iswindows() && isa(stdin, Base.TTY)
-        t = current_task()
         stdin_monitor = @async begin
             term = REPL.Terminals.TTYTerminal("xterm", stdin, stdout, stderr)
             try
@@ -229,6 +231,7 @@ try
                     # act on the results
                     if resp[1] isa Exception
                         print_testworker_errored(test, wrkr)
+                        do_quickfail && Base.throwto(t, InterruptException())
 
                         # the worker encountered some failure, recycle it
                         # so future tests get a fresh environment
@@ -258,7 +261,15 @@ catch e
                 @error "InterruptException" exception=ex,catch_backtrace()
             end
         end, all_tasks)
-    foreach(wait, all_tasks)
+    for t in all_tasks
+        # NOTE: we can't just wait, but need to discard the exception,
+        #       because the throwto for --quickfail also kills the worker.
+        try
+            wait(t)
+        catch e
+            showerror(stderr, e)
+        end
+    end
 finally
     if @isdefined stdin_monitor
         schedule(stdin_monitor, InterruptException(); error=true)
