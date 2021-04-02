e800ad0476b67c4dde952397864d22bcfea1bcc7
Tim Besard <tim@juliacomputing.com>
2021-04-02 17:22:13 +0200

    Use device overrides.

--- src/device/opencl/math.jl
+++ src/device/opencl/math.jl
@@ -11,97 +11,98 @@ const generic_types_double = [Cdouble]
 for gentype in generic_types
 @eval begin
 
-acos(x::$gentype) = @builtin_ccall("acos", $gentype, ($gentype,), x)
-acosh(x::$gentype) = @builtin_ccall("acosh", $gentype, ($gentype,), x)
-acospi(x::$gentype) = @builtin_ccall("acospi", $gentype, ($gentype,), x)
+@device_override Base.acos(x::$gentype) = @builtin_ccall("acos", $gentype, ($gentype,), x)
+@device_override Base.acosh(x::$gentype) = @builtin_ccall("acosh", $gentype, ($gentype,), x)
+@device_function acospi(x::$gentype) = @builtin_ccall("acospi", $gentype, ($gentype,), x)
 
-asin(x::$gentype) = @builtin_ccall("asin", $gentype, ($gentype,), x)
-asinh(x::$gentype) = @builtin_ccall("asinh", $gentype, ($gentype,), x)
-asinpi(x::$gentype) = @builtin_ccall("asinpi", $gentype, ($gentype,), x)
+@device_override Base.asin(x::$gentype) = @builtin_ccall("asin", $gentype, ($gentype,), x)
+@device_override Base.asinh(x::$gentype) = @builtin_ccall("asinh", $gentype, ($gentype,), x)
+@device_function asinpi(x::$gentype) = @builtin_ccall("asinpi", $gentype, ($gentype,), x)
 
-atan(y_over_x::$gentype) = @builtin_ccall("atan", $gentype, ($gentype,), y_over_x)
-atan2(y::$gentype, x::$gentype) = @builtin_ccall("atan2", $gentype, ($gentype, $gentype), y, x)
-atanh(x::$gentype) = @builtin_ccall("atanh", $gentype, ($gentype,), x)
-atanpi(x::$gentype) = @builtin_ccall("atanpi", $gentype, ($gentype,), x)
-atan2pi(y::$gentype, x::$gentype) = @builtin_ccall("atan2pi", $gentype, ($gentype, $gentype), y, x)
+@device_override Base.atan(y_over_x::$gentype) = @builtin_ccall("atan", $gentype, ($gentype,), y_over_x)
+@device_override Base.atan(y::$gentype, x::$gentype) = @builtin_ccall("atan2", $gentype, ($gentype, $gentype), y, x)
+@device_override Base.atanh(x::$gentype) = @builtin_ccall("atanh", $gentype, ($gentype,), x)
+@device_function atanpi(x::$gentype) = @builtin_ccall("atanpi", $gentype, ($gentype,), x)
+@device_function atanpi(y::$gentype, x::$gentype) = @builtin_ccall("atan2pi", $gentype, ($gentype, $gentype), y, x)
 
-cbrt(x::$gentype) = @builtin_ccall("cbrt", $gentype, ($gentype,), x)
+@device_override Base.cbrt(x::$gentype) = @builtin_ccall("cbrt", $gentype, ($gentype,), x)
 
-ceil(x::$gentype) = @builtin_ccall("ceil", $gentype, ($gentype,), x)
+@device_override Base.ceil(x::$gentype) = @builtin_ccall("ceil", $gentype, ($gentype,), x)
 
-copysign(x::$gentype, y::$gentype) = @builtin_ccall("copysign", $gentype, ($gentype, $gentype), x, y)
+@device_override Base.copysign(x::$gentype, y::$gentype) = @builtin_ccall("copysign", $gentype, ($gentype, $gentype), x, y)
 
-cos(x::$gentype) = @builtin_ccall("cos", $gentype, ($gentype,), x)
-cosh(x::$gentype) = @builtin_ccall("cosh", $gentype, ($gentype,), x)
-cospi(x::$gentype) = @builtin_ccall("cospi", $gentype, ($gentype,), x)
+@device_override Base.cos(x::$gentype) = @builtin_ccall("cos", $gentype, ($gentype,), x)
+@device_override Base.cosh(x::$gentype) = @builtin_ccall("cosh", $gentype, ($gentype,), x)
+@device_function cospi(x::$gentype) = @builtin_ccall("cospi", $gentype, ($gentype,), x)
 
-erfc(x::$gentype) = @builtin_ccall("erfc", $gentype, ($gentype,), x)
-erf(x::$gentype) = @builtin_ccall("erf", $gentype, ($gentype,), x)
+@device_override SpecialFunctions.erfc(x::$gentype) = @builtin_ccall("erfc", $gentype, ($gentype,), x)
+@device_override SpecialFunctions.erf(x::$gentype) = @builtin_ccall("erf", $gentype, ($gentype,), x)
 
-exp(x::$gentype) = @builtin_ccall("exp", $gentype, ($gentype,), x)
-exp2(x::$gentype) = @builtin_ccall("exp2", $gentype, ($gentype,), x)
-exp10(x::$gentype) = @builtin_ccall("exp10", $gentype, ($gentype,), x)
-expm1(x::$gentype) = @builtin_ccall("expm1", $gentype, ($gentype,), x)
+@device_override Base.exp(x::$gentype) = @builtin_ccall("exp", $gentype, ($gentype,), x)
+@device_override Base.exp2(x::$gentype) = @builtin_ccall("exp2", $gentype, ($gentype,), x)
+@device_override Base.exp10(x::$gentype) = @builtin_ccall("exp10", $gentype, ($gentype,), x)
+@device_override Base.expm1(x::$gentype) = @builtin_ccall("expm1", $gentype, ($gentype,), x)
 
-abs(x::$gentype) = @builtin_ccall("fabs", $gentype, ($gentype,), x)
+@device_override Base.abs(x::$gentype) = @builtin_ccall("fabs", $gentype, ($gentype,), x)
 
-fdim(x::$gentype, y::$gentype) = @builtin_ccall("fdim", $gentype, ($gentype, $gentype), x, y)
+@device_function dim(x::$gentype, y::$gentype) = @builtin_ccall("fdim", $gentype, ($gentype, $gentype), x, y)
 
-floor(x::$gentype) = @builtin_ccall("floor", $gentype, ($gentype,), x)
+@device_override Base.floor(x::$gentype) = @builtin_ccall("floor", $gentype, ($gentype,), x)
 
-fma(a::$gentype, b::$gentype, c::$gentype) = @builtin_ccall("fma", $gentype, ($gentype, $gentype, $gentype), a, b, c)
+@device_override Base.fma(a::$gentype, b::$gentype, c::$gentype) = @builtin_ccall("fma", $gentype, ($gentype, $gentype, $gentype), a, b, c)
 
-fmax(x::$gentype, y::$gentype) = @builtin_ccall("fmax", $gentype, ($gentype, $gentype), x, y)
+@device_override Base.max(x::$gentype, y::$gentype) = @builtin_ccall("fmax", $gentype, ($gentype, $gentype), x, y)
 
-fmin(x::$gentype, y::$gentype) = @builtin_ccall("fmin", $gentype, ($gentype, $gentype), x, y)
+@device_override Base.min(x::$gentype, y::$gentype) = @builtin_ccall("fmin", $gentype, ($gentype, $gentype), x, y)
 
-fmod(x::$gentype, y::$gentype) = @builtin_ccall("fmod", $gentype, ($gentype, $gentype), x, y)
+# NOTE: Julia's mod behaves differently than fmod
+#@device_override Base.mod(x::$gentype, y::$gentype) = @builtin_ccall("fmod", $gentype, ($gentype, $gentype), x, y)
 # fract(x::$gentype, $gentype *iptr) = @builtin_ccall("fract", $gentype, ($gentype, $gentype *), x, iptr)
 
-hypot(x::$gentype, y::$gentype) = @builtin_ccall("hypot", $gentype, ($gentype, $gentype), x, y)
+@device_override Base.hypot(x::$gentype, y::$gentype) = @builtin_ccall("hypot", $gentype, ($gentype, $gentype), x, y)
 
-lgamma(x::$gentype) = @builtin_ccall("lgamma", $gentype, ($gentype,), x)
+@device_override SpecialFunctions.lgamma(x::$gentype) = @builtin_ccall("lgamma", $gentype, ($gentype,), x)
 
-log(x::$gentype) = @builtin_ccall("log", $gentype, ($gentype,), x)
-log2(x::$gentype) = @builtin_ccall("log2", $gentype, ($gentype,), x)
-log10(x::$gentype) = @builtin_ccall("log10", $gentype, ($gentype,), x)
-log1p(x::$gentype) = @builtin_ccall("log1p", $gentype, ($gentype,), x)
-logb(x::$gentype) = @builtin_ccall("logb", $gentype, ($gentype,), x)
+@device_override Base.log(x::$gentype) = @builtin_ccall("log", $gentype, ($gentype,), x)
+@device_override Base.log2(x::$gentype) = @builtin_ccall("log2", $gentype, ($gentype,), x)
+@device_override Base.log10(x::$gentype) = @builtin_ccall("log10", $gentype, ($gentype,), x)
+@device_override Base.log1p(x::$gentype) = @builtin_ccall("log1p", $gentype, ($gentype,), x)
+@device_function logb(x::$gentype) = @builtin_ccall("logb", $gentype, ($gentype,), x)
 
-mad(a::$gentype, b::$gentype, c::$gentype) = @builtin_ccall("mad", $gentype, ($gentype, $gentype, $gentype), a, b, c)
+@device_function mad(a::$gentype, b::$gentype, c::$gentype) = @builtin_ccall("mad", $gentype, ($gentype, $gentype, $gentype), a, b, c)
 
-maxmag(x::$gentype, y::$gentype) = @builtin_ccall("maxmag", $gentype, ($gentype, $gentype), x, y)
-minmag(x::$gentype, y::$gentype) = @builtin_ccall("minmag", $gentype, ($gentype, $gentype), x, y)
+@device_function maxmag(x::$gentype, y::$gentype) = @builtin_ccall("maxmag", $gentype, ($gentype, $gentype), x, y)
+@device_function minmag(x::$gentype, y::$gentype) = @builtin_ccall("minmag", $gentype, ($gentype, $gentype), x, y)
 
 # modf(x::$gentype, $gentype *iptr) = @builtin_ccall("modf", $gentype, ($gentype, $gentype *), x, iptr)
 
-nextafter(x::$gentype, y::$gentype) = @builtin_ccall("nextafter", $gentype, ($gentype, $gentype), x, y)
+@device_function nextafter(x::$gentype, y::$gentype) = @builtin_ccall("nextafter", $gentype, ($gentype, $gentype), x, y)
 
-pow(x::$gentype, y::$gentype) = @builtin_ccall("pow", $gentype, ($gentype, $gentype), x, y)
-powr(x::$gentype, y::$gentype) = @builtin_ccall("powr", $gentype, ($gentype, $gentype), x, y)
+@device_override Base.:(^)(x::$gentype, y::$gentype) = @builtin_ccall("pow", $gentype, ($gentype, $gentype), x, y)
+@device_function powr(x::$gentype, y::$gentype) = @builtin_ccall("powr", $gentype, ($gentype, $gentype), x, y)
 
-remainder(x::$gentype, y::$gentype) = @builtin_ccall("remainder", $gentype, ($gentype, $gentype), x, y)
+@device_override Base.rem(x::$gentype, y::$gentype) = @builtin_ccall("remainder", $gentype, ($gentype, $gentype), x, y)
 
-rint(x::$gentype) = @builtin_ccall("rint", $gentype, ($gentype,), x)
+@device_function rint(x::$gentype) = @builtin_ccall("rint", $gentype, ($gentype,), x)
 
-round(x::$gentype) = @builtin_ccall("round", $gentype, ($gentype,), x)
+@device_override Base.round(x::$gentype) = @builtin_ccall("round", $gentype, ($gentype,), x)
 
-rsqrt(x::$gentype) = @builtin_ccall("rsqrt", $gentype, ($gentype,), x)
+@device_function rsqrt(x::$gentype) = @builtin_ccall("rsqrt", $gentype, ($gentype,), x)
 
-sin(x::$gentype) = @builtin_ccall("sin", $gentype, ($gentype,), x)
+@device_override Base.sin(x::$gentype) = @builtin_ccall("sin", $gentype, ($gentype,), x)
 # sincos(x::$gentype, $gentype *cosval) = @builtin_ccall("sincos", $gentype, ($gentype, $gentype *), x, cosval)
-sinh(x::$gentype) = @builtin_ccall("sinh", $gentype, ($gentype,), x)
-sinpi(x::$gentype) = @builtin_ccall("sinpi", $gentype, ($gentype,), x)
+@device_override Base.sinh(x::$gentype) = @builtin_ccall("sinh", $gentype, ($gentype,), x)
+@device_function sinpi(x::$gentype) = @builtin_ccall("sinpi", $gentype, ($gentype,), x)
 
-sqrt(x::$gentype) = @builtin_ccall("sqrt", $gentype, ($gentype,), x)
+@device_override Base.sqrt(x::$gentype) = @builtin_ccall("sqrt", $gentype, ($gentype,), x)
 
-tan(x::$gentype) = @builtin_ccall("tan", $gentype, ($gentype,), x)
-tanh(x::$gentype) = @builtin_ccall("tanh", $gentype, ($gentype,), x)
-tanpi(x::$gentype) = @builtin_ccall("tanpi", $gentype, ($gentype,), x)
+@device_override Base.tan(x::$gentype) = @builtin_ccall("tan", $gentype, ($gentype,), x)
+@device_override Base.tanh(x::$gentype) = @builtin_ccall("tanh", $gentype, ($gentype,), x)
+@device_function tanpi(x::$gentype) = @builtin_ccall("tanpi", $gentype, ($gentype,), x)
 
-tgamma(x::$gentype) = @builtin_ccall("tgamma", $gentype, ($gentype,), x)
+@device_function tgamma(x::$gentype) = @builtin_ccall("tgamma", $gentype, ($gentype,), x)
 
-trunc(x::$gentype) = @builtin_ccall("trunc", $gentype, ($gentype,), x)
+@device_override Base.trunc(x::$gentype) = @builtin_ccall("trunc", $gentype, ($gentype,), x)
 
 end
 end
@@ -113,8 +114,8 @@ for gentypef in generic_types_float
 
 if gentypef !== Cfloat
 @eval begin
-fmax(x::$gentypef, y::Cfloat) = @builtin_ccall("fmax", $gentypef, ($gentypef, Cfloat), x, y)
-fmin(x::$gentypef, y::Cfloat) = @builtin_ccall("fmin", $gentypef, ($gentypef, Cfloat), x, y)
+@device_override Base.max(x::$gentypef, y::Cfloat) = @builtin_ccall("fmax", $gentypef, ($gentypef, Cfloat), x, y)
+@device_override Base.min(x::$gentypef, y::Cfloat) = @builtin_ccall("fmin", $gentypef, ($gentypef, Cfloat), x, y)
 end
 end
 
@@ -127,8 +128,8 @@ for gentyped in generic_types_double
 
 if gentyped !== Cdouble
 @eval begin
-fmin(x::$gentyped, y::Cdouble) = @builtin_ccall("fmin", $gentyped, ($gentyped, Cdouble), x, y)
-fmax(x::$gentyped, y::Cdouble) = @builtin_ccall("fmax", $gentyped, ($gentyped, Cdouble), x, y)
+@device_override Base.min(x::$gentyped, y::Cdouble) = @builtin_ccall("fmin", $gentyped, ($gentyped, Cdouble), x, y)
+@device_override Base.max(x::$gentyped, y::Cdouble) = @builtin_ccall("fmax", $gentyped, ($gentyped, Cdouble), x, y)
 end
 end
 
@@ -143,16 +144,16 @@ end
 # frexp(x::Cdouble, Cint *exp) = @builtin_ccall("frexp", Cdouble, (Cdouble, Cint *), x, exp)
 
 # ilogb(x::Cfloat{n}) = @builtin_ccall("ilogb", Cint{n}, (Cfloat{n},), x)
-ilogb(x::Cfloat) = @builtin_ccall("ilogb", Cint, (Cfloat,), x)
+@device_function ilogb(x::Cfloat) = @builtin_ccall("ilogb", Cint, (Cfloat,), x)
 # ilogb(x::Cdouble{n}) = @builtin_ccall("ilogb", Cint{n}, (Cdouble{n},), x)
-ilogb(x::Cdouble) = @builtin_ccall("ilogb", Cint, (Cdouble,), x)
+@device_function ilogb(x::Cdouble) = @builtin_ccall("ilogb", Cint, (Cdouble,), x)
 
 # ldexp(x::Cfloat{n}, k::Cint{n}) = @builtin_ccall("ldexp", Cfloat{n}, (Cfloat{n}, Cint{n}), x, k)
 # ldexp(x::Cfloat{n}, k::Cint) = @builtin_ccall("ldexp", Cfloat{n}, (Cfloat{n}, Cint), x, k)
-ldexp(x::Cfloat, k::Cint) = @builtin_ccall("ldexp", Cfloat, (Cfloat, Cint), x, k)
+@device_override Base.ldexp(x::Cfloat, k::Cint) = @builtin_ccall("ldexp", Cfloat, (Cfloat, Cint), x, k)
 # ldexp(x::Cdouble{n}, k::Cint{n}) = @builtin_ccall("ldexp", Cdouble{n}, (Cdouble{n}, Cint{n}), x, k)
 # ldexp(x::Cdouble{n}, k::Cint) = @builtin_ccall("ldexp", Cdouble{n}, (Cdouble{n}, Cint), x, k)
-ldexp(x::Cdouble, k::Cint) = @builtin_ccall("ldexp", Cdouble, (Cdouble, Cint), x, k)
+@device_override Base.ldexp(x::Cdouble, k::Cint) = @builtin_ccall("ldexp", Cdouble, (Cdouble, Cint), x, k)
 
 # lgamma_r(x::Cfloat{n}, Cint{n} *signp) = @builtin_ccall("lgamma_r", Cfloat{n}, (Cfloat{n}, Cint{n} *), x, signp)
 # lgamma_r(x::Cfloat, Cint *signp) = @builtin_ccall("lgamma_r", Cfloat, (Cfloat, Cint *), x, signp)
@@ -160,14 +161,14 @@ ldexp(x::Cdouble, k::Cint) = @builtin_ccall("ldexp", Cdouble, (Cdouble, Cint), x
 # Cdouble lgamma_r(x::Cdouble, Cint *signp) = @builtin_ccall("lgamma_r", Cdouble, (Cdouble, Cint *), x, signp)
 
 # nan(nancode::uintn) = @builtin_ccall("nan", Cfloat{n}, (uintn,), nancode)
-nan(nancode::Cuint) = @builtin_ccall("nan", Cfloat, (Cuint,), nancode)
+@device_function nan(nancode::Cuint) = @builtin_ccall("nan", Cfloat, (Cuint,), nancode)
 # nan(nancode::Culong{n}) = @builtin_ccall("nan", Cdouble{n}, (Culong{n},), nancode)
-nan(nancode::Culong) = @builtin_ccall("nan", Cdouble, (Culong,), nancode)
+@device_function nan(nancode::Culong) = @builtin_ccall("nan", Cdouble, (Culong,), nancode)
 
 # pown(x::Cfloat{n}, y::Cint{n}) = @builtin_ccall("pown", Cfloat{n}, (Cfloat{n}, Cint{n}), x, y)
-pown(x::Cfloat, y::Cint) = @builtin_ccall("pown", Cfloat, (Cfloat, Cint), x, y)
+@device_override Base.:(^)(x::Cfloat, y::Cint) = @builtin_ccall("pown", Cfloat, (Cfloat, Cint), x, y)
 # pown(x::Cdouble{n}, y::Cint{n}) = @builtin_ccall("pown", Cdouble{n}, (Cdouble{n}, Cint{n}), x, y)
-pown(x::Cdouble, y::Cint) = @builtin_ccall("pown", Cdouble, (Cdouble, Cint), x, y)
+@device_override Base.:(^)(x::Cdouble, y::Cint) = @builtin_ccall("pown", Cdouble, (Cdouble, Cint), x, y)
 
 # remquo(x::Cfloat{n}, y::Cfloat{n}, Cint{n} *quo) = @builtin_ccall("remquo", Cfloat{n}, (Cfloat{n}, Cfloat{n}, Cint{n} *), x, y, quo)
 # remquo(x::Cfloat, y::Cfloat, Cint *quo) = @builtin_ccall("remquo", Cfloat, (Cfloat, Cfloat, Cint *), x::Cfloat, y, quo)
@@ -175,15 +176,9 @@ pown(x::Cdouble, y::Cint) = @builtin_ccall("pown", Cdouble, (Cdouble, Cint), x, 
 # remquo(x::Cdouble, y::Cdouble, Cint *quo) = @builtin_ccall("remquo", Cdouble, (Cdouble, Cdouble, Cint *), x, y, quo)
 
 # rootn(x::Cfloat{n}, y::Cint{n}) = @builtin_ccall("rootn", Cfloat{n}, (Cfloat{n}, Cint{n}), x, y)
-rootn(x::Cfloat, y::Cint) = @builtin_ccall("rootn", Cfloat, (Cfloat, Cint), x, y)
+@device_function rootn(x::Cfloat, y::Cint) = @builtin_ccall("rootn", Cfloat, (Cfloat, Cint), x, y)
 # rootn(x::Cdouble{n}, y::Cint{n}) = @builtin_ccall("rootn", Cdouble{n}, (Cdouble{n}, Cint{n}), x, y)
 # rootn(x::Cdouble, y::Cint) = @builtin_ccall("rootn", Cdouble{n}, (Cdouble, Cint), x, y)
 
 
 # TODO: half and native
-
-
-# temporary extensions
-
-@inline abs(x::Complex{Float64}) = hypot(x.re, x.im)
-@inline abs(x::Complex{Float32}) = hypot(x.re, x.im)
--- src/device/opencl/utils.jl
+++ src/device/opencl/utils.jl
@@ -44,6 +44,46 @@ macro builtin_ccall(name, ret, argtypes, args...)
 
     push!(opencl_builtins, mangled)
     esc(quote
-        ccall($"extern $mangled", llvmcall, $ret, ($(argtypes...),), $(args...))
+        ccall($("extern $mangled"), llvmcall, $ret, ($(argtypes...),), $(args...))
+    end)
+end
+
+
+## device overrides
+
+# local method table for device functions
+@static if isdefined(Base.Experimental, Symbol("@overlay"))
+Base.Experimental.@MethodTable(method_table)
+else
+const method_table = nothing
+end
+
+# list of overrides (only for Julia 1.6)
+const overrides = quote end
+
+macro device_override(ex)
+    code = quote
+        $GPUCompiler.@override($method_table, $ex)
+    end
+    if isdefined(Base.Experimental, Symbol("@overlay"))
+        return esc(code)
+    else
+        push!(overrides.args, code)
+        return
+    end
+end
+
+macro device_function(ex)
+    ex = macroexpand(__module__, ex)
+    def = splitdef(ex)
+
+    # generate a function that errors
+    def[:body] = quote
+        error("This function is not intended for use on the CPU")
+    end
+
+    esc(quote
+        $(combinedef(def))
+        @device_override $ex
     end)
 end
