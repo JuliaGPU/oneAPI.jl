2df770ac5dee94f1a3198598c19e099f5294637c
Tim Besard <tim.besard@gmail.com>
2021-08-12 15:41:55 +0200

    Add high-level wrapper for gemm.

--- lib/mkl/wrappers.jl
+++ lib/mkl/wrappers.jl
@@ -14,3 +14,86 @@ Base.unsafe_convert(::Type{<:ZeCxxPtr{T}}, x::oneArray{T}) where {T} =
 function __init__()
   @initcxx
 end
+
+
+#
+# Auxiliary
+#
+
+function Base.convert(::Type{onemklTranspose}, trans::Char)
+    if trans == 'N'
+        return ONEMKL_TRANSPOSE_NONTRANS
+    elseif trans == 'T'
+        return ONEMKL_TRANSPOSE_TRANS
+    elseif trans == 'C'
+        return ONEMLK_TRANSPOSE_CONJTRANS
+    else
+        throw(ArgumentError("Unknown transpose $trans"))
+    end
+end
+
+
+
+#
+# BLAS
+#
+
+# level 3
+
+for (fname, elty) in
+        ((:onemklDgemm,:Float64),
+         (:onemklSgemm,:Float32),
+         (:onemklHgemm, :Float16),
+         (:onemklZgemm,:ComplexF64),
+         (:onemklCgemm,:ComplexF32))
+    @eval begin
+        function gemm!(transA::Char,
+                       transB::Char,
+                       alpha::Number,
+                       A::oneVecOrMat{$elty},
+                       B::oneVecOrMat{$elty},
+                       beta::Number,
+                       C::oneVecOrMat{$elty})
+            m = size(A, transA == 'N' ? 1 : 2)
+            k = size(A, transA == 'N' ? 2 : 1)
+            n = size(B, transB == 'N' ? 2 : 1)
+            if m != size(C,1) || n != size(C,2) || k != size(B, transB == 'N' ? 1 : 2)
+                throw(DimensionMismatch(""))
+            end
+
+            lda = max(1,stride(A,2))
+            ldb = max(1,stride(B,2))
+            ldc = max(1,stride(C,2))
+
+            device(A) == device(B) == device(C) || error("Multi-device GEMM not supported")
+            context(A) == context(B) == context(C) || error("Multi-context GEMM not supported")
+            queue = global_queue(context(A), device(A))
+
+            # FIXME: CxxWrap-generated function signatures only accept Integers
+            #        for enum-typed arguments, even though that conversion
+            #        should only be done at the `ccall` site.
+            transA = convert(onemklTranspose, transA)
+            transB = convert(onemklTranspose, transB)
+
+            $fname(sycl_queue(queue), transA, transB, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc)
+            C
+        end
+
+        function gemm(transA::Char,
+                      transB::Char,
+                      alpha::Number,
+                      A::oneVecOrMat{$elty},
+                      B::oneVecOrMat{$elty})
+            gemm!(transA, transB, alpha, A, B, zero($elty),
+                  similar(B, $elty, (size(A, transA == 'N' ? 1 : 2),
+                                     size(B, transB == 'N' ? 2 : 1))))
+        end
+
+        function gemm(transA::Char,
+                      transB::Char,
+                      A::oneVecOrMat{$elty},
+                      B::oneVecOrMat{$elty})
+            gemm(transA, transB, one($elty), A, B)
+        end
+    end
+end
