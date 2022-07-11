14abca958ae57d184609042a7c650ec11fa913ea
Tim Besard <tim.besard@gmail.com>
2022-07-11 11:30:39 +0200

    Rework build script: Use CMake JLL and scratch spaces.

--- deps/build.jl
+++ deps/build.jl
@@ -2,43 +2,37 @@
 
 # XXX: build this library on Yggdrasil
 
-using Pkg
-Pkg.instantiate()
+using Scratch, CMake_jll, oneAPI_Level_Zero_Headers_jll
 
+oneAPI = Base.UUID("8f75cd03-7ff8-4ecb-9b8f-daf728133b1b")
 
-# XXX: this still assumes build essentials are installed
-if haskey(ENV, "CI")
-    run(`apt update`)
-    run(`apt install -y build-essential cmake`)
-end
-
-
-using Conda
+# get scratch directories
+conda_dir = get_scratch!(oneAPI, "conda")
+install_dir = get_scratch!(oneAPI, "deps")
+rm(install_dir; recursive=true)
 
 # install the toolchain
-Conda.add("dpcpp_linux-64", :oneapi; channel="intel")
-Conda.add("mkl-devel-dpcpp", :oneapi; channel="intel")
-
-
-using oneAPI_Level_Zero_Headers_jll
+using Conda
+if !isfile(joinpath(conda_dir, "condarc-julia.yml"))
+    Conda.create(conda_dir)
+    # conda#8850
+    mkpath(joinpath(conda_dir, "conda-meta"))
+    touch(joinpath(conda_dir, "conda-meta", "history"))
+end
+Conda.add("dpcpp_linux-64", conda_dir; channel="intel")
+Conda.add("mkl-devel-dpcpp", conda_dir; channel="intel")
 
 # XXX: isn't there a Conda package providing ze_api.hpp?
 include_dir =  joinpath(oneAPI_Level_Zero_Headers_jll.artifact_dir, "include")
 
-rpath = String[]
-push!(rpath, Conda.lib_dir(:oneapi))
-
-withenv("PATH"=>"$(ENV["PATH"]):$(Conda.bin_dir(:oneapi))",
-        "LD_LIBRARY_PATH"=>Conda.lib_dir(:oneapi)) do
+withenv("PATH"=>"$(ENV["PATH"]):$(Conda.bin_dir(conda_dir))",
+        "LD_LIBRARY_PATH"=>Conda.lib_dir(conda_dir)) do
 mktempdir() do build_dir
-mktempdir() do install_dir
-    run(```cmake -DCMAKE_CXX_FLAGS="-isystem $include_dir"
-                 -DCMAKE_INSTALL_RPATH=$(join(rpath, ';'))
-                 -DCMAKE_INSTALL_PREFIX=$install_dir
-                 -S $(@__DIR__) -B $build_dir```)
-    run(`make -C $build_dir -j $(Sys.CPU_THREADS) VERBOSE=true install`)
-    mv(joinpath(install_dir, "lib", "liboneapilib.so"),
-       joinpath(@__DIR__, "liboneapilib.so"); force=true)
-end
+    run(```$(cmake()) -DCMAKE_CXX_FLAGS="-isystem $include_dir"
+                      -DCMAKE_INSTALL_RPATH=$(Conda.lib_dir(conda_dir))
+                      -DCMAKE_INSTALL_PREFIX=$install_dir
+                      -S $(@__DIR__) -B $build_dir```)
+    run(`$(cmake()) --build $(build_dir) --parallel $(Sys.CPU_THREADS)`)
+    run(`$(cmake()) --install $(build_dir)`)
 end
 end
