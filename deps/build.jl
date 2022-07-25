cf95d974d666f95cbc9f3d768bea665d8c8c40f9
Tim Besard <tim.besard@gmail.com>
2022-07-25 15:35:41 +0200

    Debug.

--- deps/build.jl
+++ deps/build.jl
@@ -24,6 +24,8 @@ Conda.add("mkl-devel-dpcpp", conda_dir; channel="intel")
 
 Conda.list(conda_dir)
 
+run(pipeline(`nm -D $(Conda.lib_dir(conda_dir))/libmkl_sycl.so`, `grep clean_device_info`))
+
 # XXX: isn't there a Conda package providing ze_api.hpp?
 include_dir =  joinpath(oneAPI_Level_Zero_Headers_jll.artifact_dir, "include")
 
