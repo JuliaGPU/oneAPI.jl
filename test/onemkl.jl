5180b9b79a5bef237e593515cdd9f674365afb39
Kali Uday <kali.uday.balleda@intel.com>
2022-10-31 22:13:03 +0530

    scal test case updated in onemkl.jl

--- test/onemkl.jl
+++ test/onemkl.jl
@@ -1,11 +1,11 @@
-#using oneAPI.oneMKL
-#using LinearAlgebra
+using oneAPI.oneMKL
+using LinearAlgebra
 
 m = 20
 n = 35
 k = 13
 
-const eltypes=[Float32, ComplexF32]
+eltypes=[Float32, ComplexF32]
 const float64_supported = oneL0.module_properties(device()).fp64flags & oneL0.ZE_DEVICE_MODULE_FLAG_FP64 == oneL0.ZE_DEVICE_MODULE_FLAG_FP64
 if (float64_supported)
     append!(eltypes, [Float64, ComplexF64])
@@ -14,17 +14,14 @@ end
 ########################
 @testset "level 1" begin
 	@testset for T in eltypes
-	    A = oneArray(rand(T, m))
-	    B = A
-	    alpha = 2
-	    B = B.* 2
-	    println("CPU Result")
-	    @show B
-
-	    oneMKL.scal!(m, alpha, A)
-	    println("GPU Result")
-	    @show A
-
-	    @test Array(A) == Array(B)
+		A = rand(T, m)
+		gpuA = oneArray(A)
+		if T === Float32
+			oneMKL.scal!(m, 5f0, gpuA)
+		else
+			oneMKL.scal!(m, 5.0, gpuA)
+		end
+		_A = Array(gpuA)
+	    @test isapprox(A .* 5, _A)
 	end
 end
