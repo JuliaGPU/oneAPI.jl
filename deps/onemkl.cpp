86f9dc73d259b89d8233145df5d811a6727f3cb5
Kali Uday <kali.uday.balleda@intel.com>
2022-10-31 22:26:27 +0530

    indentation NITS

--- deps/onemkl.cpp
+++ deps/onemkl.cpp
@@ -8,25 +8,25 @@
 // https://spec.oneapi.io/versions/1.0-rev-1/elements/oneMKL/source/domains/blas/gemm.html
 
 oneapi::mkl::transpose convert(onemklTranspose val) {
-  switch (val) {
-  case ONEMKL_TRANSPOSE_NONTRANS:
-    return oneapi::mkl::transpose::nontrans;
-  case ONEMKL_TRANSPOSE_TRANS:
-    return oneapi::mkl::transpose::trans;
-  case ONEMLK_TRANSPOSE_CONJTRANS:
-    return oneapi::mkl::transpose::conjtrans;
-  }
+    switch (val) {
+    case ONEMKL_TRANSPOSE_NONTRANS:
+        return oneapi::mkl::transpose::nontrans;
+    case ONEMKL_TRANSPOSE_TRANS:
+        return oneapi::mkl::transpose::trans;
+    case ONEMLK_TRANSPOSE_CONJTRANS:
+        return oneapi::mkl::transpose::conjtrans;
+    }
 }
 
 extern "C" int onemklHgemm(syclQueue_t device_queue, onemklTranspose transA,
                            onemklTranspose transB, int64_t m, int64_t n,
-                           int64_t k, sycl::half alpha, const sycl::half *A,
-                           int64_t lda, const sycl::half *B, int64_t ldb,
-                           sycl::half beta, sycl::half *C, int64_t ldc) {
-  oneapi::mkl::blas::column_major::gemm(device_queue->val, convert(transA),
-                                        convert(transB), m, n, k, alpha, A, lda,
-                                        B, ldb, beta, C, ldc);
-  return 0;
+                           int64_t k, sycl::half alpha, const sycl::half *A, int64_t lda,
+                           const sycl::half *B, int64_t ldb, sycl::half beta, sycl::half *C,
+                           int64_t ldc) {
+    oneapi::mkl::blas::column_major::gemm(device_queue->val, convert(transA),
+                                          convert(transB), m, n, k, alpha, A,
+                                          lda, B, ldb, beta, C, ldc);
+    return 0;
 }
 
 extern "C" int onemklSgemm(syclQueue_t device_queue, onemklTranspose transA,
@@ -34,10 +34,10 @@ extern "C" int onemklSgemm(syclQueue_t device_queue, onemklTranspose transA,
                            int64_t k, float alpha, const float *A, int64_t lda,
                            const float *B, int64_t ldb, float beta, float *C,
                            int64_t ldc) {
-  oneapi::mkl::blas::column_major::gemm(device_queue->val, convert(transA),
-                                        convert(transB), m, n, k, alpha, A, lda,
-                                        B, ldb, beta, C, ldc);
-  return 0;
+    oneapi::mkl::blas::column_major::gemm(device_queue->val, convert(transA),
+                                          convert(transB), m, n, k, alpha, A,
+                                          lda, B, ldb, beta, C, ldc);
+    return 0;
 }
 
 extern "C" int onemklDgemm(syclQueue_t device_queue, onemklTranspose transA,
@@ -45,10 +45,10 @@ extern "C" int onemklDgemm(syclQueue_t device_queue, onemklTranspose transA,
                            int64_t k, double alpha, const double *A,
                            int64_t lda, const double *B, int64_t ldb,
                            double beta, double *C, int64_t ldc) {
-  oneapi::mkl::blas::column_major::gemm(device_queue->val, convert(transA),
-                                        convert(transB), m, n, k, alpha, A, lda,
-                                        B, ldb, beta, C, ldc);
-  return 0;
+    oneapi::mkl::blas::column_major::gemm(device_queue->val, convert(transA),
+                                          convert(transB), m, n, k, alpha, A,
+                                          lda, B, ldb, beta, C, ldc);
+    return 0;
 }
 
 extern "C" int onemklCgemm(syclQueue_t device_queue, onemklTranspose transA,
@@ -58,12 +58,12 @@ extern "C" int onemklCgemm(syclQueue_t device_queue, onemklTranspose transA,
                            const float _Complex *B, int64_t ldb,
                            float _Complex beta, float _Complex *C,
                            int64_t ldc) {
-  oneapi::mkl::blas::column_major::gemm(
-      device_queue->val, convert(transA), convert(transB), m, n, k, alpha,
-      reinterpret_cast<const std::complex<float> *>(A), lda,
-      reinterpret_cast<const std::complex<float> *>(B), ldb, beta,
-      reinterpret_cast<std::complex<float> *>(C), ldc);
-  return 0;
+    oneapi::mkl::blas::column_major::gemm(
+        device_queue->val, convert(transA), convert(transB), m, n, k, alpha,
+        reinterpret_cast<const std::complex<float> *>(A), lda,
+        reinterpret_cast<const std::complex<float> *>(B), ldb, beta,
+        reinterpret_cast<std::complex<float> *>(C), ldc);
+    return 0;
 }
 
 extern "C" int onemklZgemm(syclQueue_t device_queue, onemklTranspose transA,
@@ -73,54 +73,29 @@ extern "C" int onemklZgemm(syclQueue_t device_queue, onemklTranspose transA,
                            const double _Complex *B, int64_t ldb,
                            double _Complex beta, double _Complex *C,
                            int64_t ldc) {
-  oneapi::mkl::blas::column_major::gemm(
-      device_queue->val, convert(transA), convert(transB), m, n, k, alpha,
-      reinterpret_cast<const std::complex<double> *>(A), lda,
-      reinterpret_cast<const std::complex<double> *>(B), ldb, beta,
-      reinterpret_cast<std::complex<double> *>(C), ldc);
-  return 0;
+    oneapi::mkl::blas::column_major::gemm(
+        device_queue->val, convert(transA), convert(transB), m, n, k, alpha,
+        reinterpret_cast<const std::complex<double> *>(A), lda,
+        reinterpret_cast<const std::complex<double> *>(B), ldb, beta,
+        reinterpret_cast<std::complex<double> *>(C), ldc);
+    return 0;
 }
 
-// Support Level-1: SCAL primitive
-extern "C" void onemklDscal(syclQueue_t device_queue, int64_t n, double alpha,
-                            double *x, int64_t incx) {
-  oneapi::mkl::blas::column_major::scal(device_queue->val, n, alpha, x, incx);
-}
-
-extern "C" void onemklSscal(syclQueue_t device_queue, int64_t n, float alpha,
-                            float *x, int64_t incx) {
-  oneapi::mkl::blas::column_major::scal(device_queue->val, n, alpha, x, incx);
-}
-
-extern "C" void onemklCscal(syclQueue_t device_queue, int64_t n,
-                            float alpha, float _Complex *x,
-                            int64_t incx) {
-  oneapi::mkl::blas::column_major::scal(
-      device_queue->val, n, alpha, reinterpret_cast<std::complex<float> *>(x),
-      incx);
-}
-
-extern "C" void onemklZscal(syclQueue_t device_queue, int64_t n,
-                            double alpha, double _Complex *x,
-                            int64_t incx) {
-  oneapi::mkl::blas::column_major::scal(
-      device_queue->val, n, alpha, reinterpret_cast<std::complex<double> *>(x),
-      incx);
-}
 
 // other
 
-// oneMKL keeps a cache of SYCL queues and tries to destroy them when unloading
-// the library. that is incompatible with oneAPI.jl destroying queues before
-// that, so expose a function to manually wipe the device cache when we're
-// destroying queues.
+// oneMKL keeps a cache of SYCL queues and tries to destroy them when unloading the library.
+// that is incompatible with oneAPI.jl destroying queues before that, so expose a function
+// to manually wipe the device cache when we're destroying queues.
 
 namespace oneapi {
 namespace mkl {
 namespace gpu {
 int clean_gpu_caches();
 }
-} // namespace mkl
-} // namespace oneapi
+}
+}
 
-extern "C" void onemklDestroy() { oneapi::mkl::gpu::clean_gpu_caches(); }
+extern "C" void onemklDestroy() {
+    oneapi::mkl::gpu::clean_gpu_caches();
+}
