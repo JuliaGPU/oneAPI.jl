990300a65a7f30cfc5d0e712009642781029f335
Kali Uday <kali.uday.balleda@intel.com>
2022-10-31 23:05:04 +0530

    cleanup onemkl.jl

--- test/onemkl.jl
+++ test/onemkl.jl
@@ -5,11 +5,6 @@ m = 20
 n = 35
 k = 13
 
-eltypes=[Float32, ComplexF32]
-const float64_supported = oneL0.module_properties(device()).fp64flags & oneL0.ZE_DEVICE_MODULE_FLAG_FP64 == oneL0.ZE_DEVICE_MODULE_FLAG_FP64
-if (float64_supported)
-    append!(eltypes, [Float64, ComplexF64])
-end
 
 ########################
 @testset "level 1" begin
