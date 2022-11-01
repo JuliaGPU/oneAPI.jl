139de1c9c9a06beab97f8bc122127ba8cdfe1dbd
Kali Uday <kali.uday.balleda@intel.com>
2022-11-01 21:49:23 +0530

    NITS

--- test/onemkl.jl
+++ test/onemkl.jl
@@ -14,24 +14,23 @@ k = 13
 		gpuA = oneArray(A)
 		gpuB = oneArray(A)
 ##### Failed Cases
+#		Following test fails if we use our own rmul! implementation from lib/mkl/linalg.jl
 #		@test testf(rmul!, gpuA, Ref(alpha[1]))
 #		@test testf(oneMKL.scal!, m, alpha[1], gpuA)
-#		oneMKL.scal!(m, alpha[1], gpuA)
-#		_A = Array(gpuA)
-#		@test isapprox(A .* alpha[1], _A)
 
 #### Pass Cases
 		# TODO: This test passes if we disable our own implementation 
 		# of rmul in lib/mkl/linalg.jl which means rmul! cpu implmentation might 
 		# be taking over it ?
 		@test testf(rmul!, gpuB, Ref(alpha[1]))
-		
+
+		# This test works fine
+		# It manually checks for CPU/GPU comparisons for scal primitive
 		if T === Float32
 			oneMKL.scal!(m, 5f0, gpuA)
 		else 
 			oneMKL.scal!(m, 5.0, gpuA)
 		end
-
 		_A = Array(gpuA)
 		@test isapprox(A .* 5, _A)
 
