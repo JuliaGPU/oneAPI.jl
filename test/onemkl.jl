be028001535aeeecc9ba31708cb1a51a789b8088
Kali Uday <kali.uday.balleda@intel.com>
2022-11-08 16:31:14 +0530

    added test set for iamax/iamin

--- test/onemkl.jl
+++ test/onemkl.jl
@@ -20,11 +20,13 @@ m = 20
             alpha = rand(T,1)
             @test testf(axpy!, alpha[1], rand(T,m), rand(T,m))
         end
-
-        # testing oneMKL max and min
-        a = convert.(T, [1.0, 2.0, -0.8, 5.0, 3.0])
-        ca = oneArray(a)
-        @test BLAS.iamax(a)  == oneMKL.iamax(ca)
-        @test oneMKL.iamin(ca) == 3
+		
+		@testset "iamax/iamin" begin
+		    # testing oneMKL max and min
+		    a = convert.(T, [1.0, 2.0, -0.8, 5.0, 3.0])
+		    ca = oneArray(a)
+		    @test BLAS.iamax(a)  == oneMKL.iamax(ca)
+		    @test oneMKL.iamin(ca) == 3
+		end
     end # level 1 testset
 end
