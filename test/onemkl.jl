038252f515ea53377e239f9df4fe36cbab4835e7
Kali Uday <kali.uday.balleda@intel.com>
2022-11-08 13:58:27 +0530

    NITS

--- test/onemkl.jl
+++ test/onemkl.jl
@@ -19,25 +19,25 @@ k = 13
         
         @testset "scal" begin
             # Test scal primitive [alpha/x: F32, F64, CF32, CF64]
-		    alpha = rand(T,1)
-		    @test testf(rmul!, rand(T,m), alpha[1])
+            alpha = rand(T,1)
+            @test testf(rmul!, rand(T,m), alpha[1])
 
-		    # Test scal primitive [alpha - F32, F64, x - CF32, CF64] 
-		    A = rand(T,m)
-		    gpuA = oneArray(A)
-		    if T === ComplexF32
-			    alphaf32 = rand(Float32, 1)
-			    B = A .* alphaf32[1]
-			    oneMKL.scal!(m, alphaf32[1], gpuA)
-			    @test Array(B) == Array(gpuA)
-		    end
+            # Test scal primitive [alpha - F32, F64, x - CF32, CF64] 
+            A = rand(T,m)
+            gpuA = oneArray(A)
+            if T === ComplexF32
+                alphaf32 = rand(Float32, 1)
+                B = A .* alphaf32[1]
+                oneMKL.scal!(m, alphaf32[1], gpuA)
+                @test Array(B) == Array(gpuA)
+            end
 
-		    if T === ComplexF64
-			    alphaf64 = rand(Float64, 1)
-			    B = A .* alphaF64[1]
-			    oneMKL.scal!(m, alphaf64[1], gpuA)
-			    @test Array(B) == Array(gpuA)
-		    end
+            if T === ComplexF64
+                alphaf64 = rand(Float64, 1)
+                B = A .* alphaF64[1]
+                oneMKL.scal!(m, alphaf64[1], gpuA)
+                @test Array(B) == Array(gpuA)
+            end
 		    
         end
 		
