3a13c9fb8d4331542c8046693cf9a34037f7c4f6
Kali Uday <kali.uday.balleda@intel.com>
2022-11-08 13:28:11 +0530

    use testset for copy/axpy

--- test/onemkl.jl
+++ test/onemkl.jl
@@ -10,13 +10,17 @@ k = 13
 ############################################################################################
 @testset "level 1" begin
     @testset for T in intersect(eltypes, [Float32, Float64, ComplexF32, ComplexF64])
-        A = oneArray(rand(T, m))
-        B = oneArray{T}(undef, m)
-        oneMKL.copy!(m,A,B)
-        @test Array(A) == Array(B)
-		
-		# Test axpy primitive
-		alpha = rand(T,1)
-		@test testf(axpy!, alpha[1], rand(T,m), rand(T,m))
+        @testset "copy" begin
+            A = oneArray(rand(T, m))
+            B = oneArray{T}(undef, m)
+            oneMKL.copy!(m,A,B)
+            @test Array(A) == Array(B)
+		end 
+        
+        @testset "axpy" begin
+            # Test axpy primitive
+            alpha = rand(T,1)
+            @test testf(axpy!, alpha[1], rand(T,m), rand(T,m))
+        end
     end # level 1 testset
 end
