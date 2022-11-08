d07339ea3a62a51a40457d9f307e691c70ba8c17
Kali Uday <kali.uday.balleda@intel.com>
2022-11-08 14:42:37 +0530

    Cleanup of tests

--- test/onemkl.jl
+++ test/onemkl.jl
@@ -27,19 +27,15 @@ k = 13
             gpuA = oneArray(A)
             if T === ComplexF32
                 alphaf32 = rand(Float32, 1)
-                B = A .* alphaf32[1]
                 oneMKL.scal!(m, alphaf32[1], gpuA)
-                @test Array(B) == Array(gpuA)
+                @test isapprox(A .* alphaf32[1], Array(gpuA))
             end
 
             if T === ComplexF64
                 alphaf64 = rand(Float64, 1)
-                B = A .* alphaF64[1]
                 oneMKL.scal!(m, alphaf64[1], gpuA)
-                @test Array(B) == Array(gpuA)
-            end
-		    
+                @test isapprox(A .* alphaf64[1], Array(gpuA))
+            end	    
         end
-		
     end # level 1 testset
 end
