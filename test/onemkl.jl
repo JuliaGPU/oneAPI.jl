fe163c45be609d891c27d1a6328a2faef321a799
Tim Besard <tim.besard@gmail.com>
2022-11-10 17:42:58 +0100

    Improve oneMKL tests.

--- test/onemkl.jl
+++ test/onemkl.jl
@@ -8,35 +8,32 @@ m = 20
 ############################################################################################
 @testset "level 1" begin
     @testset for T in intersect(eltypes, [Float32, Float64, ComplexF32, ComplexF64])
-        A = oneArray(rand(T, m))
-        B = oneArray{T}(undef, m)
-        oneMKL.copy!(m,A,B)
-        @test Array(A) == Array(B)
+        @testset "copy" begin
+            A = oneArray(rand(T, m))
+            B = oneArray{T}(undef, m)
+            oneMKL.copy!(m,A,B)
+            @test Array(A) == Array(B)
+        end
 
         @testset "nrm2" begin
-            # Test nrm2 primitive
             @test testf(norm, rand(T,m))
-        end # end of nrm2
+        end
 
         @testset "iamax/iamin" begin
-            # testing oneMKL max and min
             a = convert.(T, [1.0, 2.0, -0.8, 5.0, 3.0])
             ca = oneArray(a)
-            @test BLAS.iamax(a)  == oneMKL.iamax(ca)
+            @test BLAS.iamax(a) == oneMKL.iamax(ca)
             @test oneMKL.iamin(ca) == 3
-        end # end of iamax/iamin
+        end
 
         @testset "swap" begin
-            # testing swap
             x = rand(T, m)
             y = rand(T, m)
             dx = oneArray(x)
             dy = oneArray(y)
             oneMKL.swap!(m, dx, dy)
-            h_x = collect(dx)
-            h_y = collect(dy)
-            @test h_x ≈ y
-            @test h_y ≈ x
-        end # end of swap
-    end # level 1 testset
+            @test Array(dx) == y
+            @test Array(dy) == x
+        end
+    end
 end
