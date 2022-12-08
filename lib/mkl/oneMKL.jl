83bd8d4184fe420898861282bb808f664970fbc9
Kali Uday Balleda <kali.uday.balleda@intel.com>
2022-12-08 15:43:23 +0530

    oneMKL: Add support for L1 dot and many L2 primitives. (#253)

--- lib/mkl/oneMKL.jl
+++ lib/mkl/oneMKL.jl
@@ -18,4 +18,35 @@ const onemklFloat = Union{Float64,Float32,ComplexF64,ComplexF32}
 include("wrappers.jl")
 include("linalg.jl")
 
+function band(A::StridedArray, kl, ku)
+    m, n = size(A)
+    AB = zeros(eltype(A),kl+ku+1,n)
+    for j = 1:n
+        for i = max(1,j-ku):min(m,j+kl)
+            AB[ku+1-j+i,j] = A[i,j]
+        end
+    end
+    return AB
+end
+
+# convert band storage to general matrix
+function unband(AB::StridedArray,m,kl,ku)
+    bm, n = size(AB)
+    A = zeros(eltype(AB),m,n)
+    for j = 1:n
+        for i = max(1,j-ku):min(m,j+kl)
+            A[i,j] = AB[ku+1-j+i,j]
+        end
+    end
+    return A
+end
+
+# zero out elements not on matrix bands
+function bandex(A::AbstractMatrix,kl,ku)
+    m, n = size(A)
+    AB = band(A,kl,ku)
+    B = unband(AB,m,kl,ku)
+    return B
+end
+
 end
