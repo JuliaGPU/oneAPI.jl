44390349a456bbd5c851ce5279e8fb7951354496
Tim Besard <tim.besard@gmail.com>
2023-01-26 09:16:24 +0100

    Switch to icpx compiler.

--- deps/build_local.jl
+++ deps/build_local.jl
@@ -46,13 +46,13 @@ Conda.add("mkl-devel-dpcpp", conda_dir; channel="intel")
 Conda.list(conda_dir)
 
 # XXX: isn't there a Conda package providing ze_api.hpp?
-include_dir =  joinpath(oneAPI_Level_Zero_Headers_jll.artifact_dir, "include")
+include_dir = joinpath(oneAPI_Level_Zero_Headers_jll.artifact_dir, "include")
 
 # build and install
 withenv("PATH"=>"$(ENV["PATH"]):$(Conda.bin_dir(conda_dir))",
         "LD_LIBRARY_PATH"=>Conda.lib_dir(conda_dir)) do
-    run(```$(cmake()) -DCMAKE_CXX_COMPILER="dpcpp"
-                      -DCMAKE_CXX_FLAGS="-isystem $include_dir"
+    run(```$(cmake()) -DCMAKE_CXX_COMPILER="icpx"
+                      -DCMAKE_CXX_FLAGS="-isystem $(conda_dir)/include -isystem $include_dir"
                       -DCMAKE_INSTALL_RPATH=$(Conda.lib_dir(conda_dir))
                       -DCMAKE_INSTALL_PREFIX=$install_dir
                       -S $(@__DIR__) -B $build_dir```)
