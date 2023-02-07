17a56b607ed26fe05c42bbb3adbfd7cef673dd2e
Tu, Peng <peng.tu@intel.com>
2023-02-07 13:16:10 -0800

    Update onemklDestory() to call the public mkl_free_buffers() to clean up with oneMKL 2023.0.0

--- deps/src/onemkl.cpp
+++ deps/src/onemkl.cpp
@@ -1098,17 +1098,8 @@ extern "C" void onemklZswap(syclQueue_t device_queue, int64_t n, double _Complex
 // other
 
 // oneMKL keeps a cache of SYCL queues and tries to destroy them when unloading the library.
-// that is incompatible with oneAPI.jl destroying queues before that, so expose a function
+// that is incompatible with oneAPI.jl destroying queues before that, so call mkl_free_buffers()
 // to manually wipe the device cache when we're destroying queues.
-
-namespace oneapi {
-namespace mkl {
-namespace gpu {
-int clean_gpu_caches();
-}
-}
-}
-
 extern "C" void onemklDestroy() {
-    oneapi::mkl::gpu::clean_gpu_caches();
+    mkl_free_buffers();
 }
