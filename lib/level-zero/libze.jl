5e68b47e66dd3c8fe4ad026d7301b1149760229f
Tim Besard <tim.besard@gmail.com>
2023-06-05 16:49:50 +0200

    Replace macros with functions to cut down on generated code. (#329)

--- lib/level-zero/libze.jl
+++ lib/level-zero/libze.jl
@@ -2,24 +2,24 @@ using CEnum
 
 # outlined functionality to avoid GC frame allocation
 @noinline function throw_api_error(res)
-    throw(ZeError(res))
+    if res == RESULT_ERROR_OUT_OF_HOST_MEMORY || res == RESULT_ERROR_OUT_OF_DEVICE_MEMORY
+        throw(OutOfGPUMemoryError())
+    else
+        throw(ZeError(res))
+    end
 end
 
-macro check(ex)
-    is_oom = :(isequal(res, RESULT_ERROR_OUT_OF_HOST_MEMORY) ||
-               isequal(res, RESULT_ERROR_OUT_OF_DEVICE_MEMORY))
-
-    quote
-        res = @retry_reclaim res -> $is_oom $(esc(ex))
-
-        if $is_oom
-            throw(OutOfMemoryError())
-        elseif res != RESULT_SUCCESS
-            throw_api_error(res)
-        end
-
-        return
+function check(f)
+    res = retry_reclaim(err -> err == RESULT_ERROR_OUT_OF_HOST_MEMORY ||
+                               err == RESULT_ERROR_OUT_OF_DEVICE_MEMORY) do
+        f()
     end
+
+    if res != RESULT_SUCCESS
+        throw_api_error(res)
+    end
+
+    return
 end
 
 const ze_bool_t = UInt8
