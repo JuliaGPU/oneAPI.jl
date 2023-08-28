03c1bbe4eeb38cbc361ef5bea4b39ea604da4671
Tim Besard <tim.besard@gmail.com>
2023-08-28 13:06:21 +0200

    Move to Julia 1.8.

--- src/device/utils.jl
+++ src/device/utils.jl
@@ -82,25 +82,12 @@ end
 ## device overrides
 
 # local method table for device functions
-@static if isdefined(Base.Experimental, Symbol("@overlay"))
 Base.Experimental.@MethodTable(method_table)
-else
-const method_table = nothing
-end
-
-# list of overrides (only for Julia 1.6)
-const overrides = quote end
 
 macro device_override(ex)
-    code = quote
-        $GPUCompiler.@override($method_table, $ex)
-    end
-    if isdefined(Base.Experimental, Symbol("@overlay"))
-        return esc(code)
-    else
-        push!(overrides.args, code)
-        return
-    end
+    esc(quote
+        Base.Experimental.@overlay($method_table, $ex)
+    end)
 end
 
 macro device_function(ex)
