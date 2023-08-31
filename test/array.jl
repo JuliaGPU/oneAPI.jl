cae94efbff174a879e2dacdd339c32d06a9b36c4
Tim Besard <tim.besard@gmail.com>
2023-08-31 17:29:34 +0200

    Switch to GPUArrays buffer management.

--- test/array.jl
+++ test/array.jl
@@ -4,18 +4,18 @@ import Adapt
 @testset "constructors" begin
   xs = oneArray{Int}(undef, 2, 3)
   @test collect(oneArray([1 2; 3 4])) == [1 2; 3 4]
-  @test testf(vec, rand(5,3))
+  @test testf(vec, rand(Float32, 5,3))
   @test Base.elsize(xs) == sizeof(Int)
   @test oneArray{Int, 2}(xs) === xs
 
   @test_throws ArgumentError Base.unsafe_convert(Ptr{Int}, xs)
   @test_throws ArgumentError Base.unsafe_convert(Ptr{Float32}, xs)
 
-  @test collect(oneAPI.zeros(2, 2)) == zeros(2, 2)
-  @test collect(oneAPI.ones(2, 2)) == ones(2, 2)
+  @test collect(oneAPI.zeros(Float32, 2, 2)) == zeros(Float32, 2, 2)
+  @test collect(oneAPI.ones(Float32, 2, 2)) == ones(Float32, 2, 2)
 
-  @test collect(oneAPI.fill(0, 2, 2)) == zeros(2, 2)
-  @test collect(oneAPI.fill(1, 2, 2)) == ones(2, 2)
+  @test collect(oneAPI.fill(0, 2, 2)) == zeros(Int, 2, 2)
+  @test collect(oneAPI.fill(1, 2, 2)) == ones(Int, 2, 2)
 end
 
 @testset "adapt" begin
