1d4554462ea16eade18e63aae49f156846b59e10
Tu, Peng <peng.tu@intel.com>
2024-04-12 10:39:12 -0700

    Another attempt to use mkl_free_buffers

--- deps/src/onemkl.cpp
+++ deps/src/onemkl.cpp
@@ -4319,18 +4319,10 @@ extern "C" int onemklXsparse_matmat(syclQueue_t device_queue, matrix_handle_t A,
 // other
 
 // oneMKL keeps a cache of SYCL queues and tries to destroy them when unloading the library.
-// that is incompatible with oneAPI.jl destroying queues before that, so expose a function
+// that is incompatible with oneAPI.jl destroying queues before that, so call mkl_free_buffers
 // to manually wipe the device cache when we're destroying queues.
 
-namespace oneapi {
-namespace mkl {
-namespace gpu {
-int clean_gpu_caches();
-}
-}
-}
-
 extern "C" int onemklDestroy() {
-    oneapi::mkl::gpu::clean_gpu_caches();
+    mkl_free_buffers();
     return 0;
 }
