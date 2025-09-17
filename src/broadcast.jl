9915f0389c2584e13101b6515b333f3bb1657422
Michel Schanen <michel.schanen@gmail.com>
2025-09-17 14:27:02 -0500

    Fix Julia 1.12 issues

--- src/broadcast.jl
+++ src/broadcast.jl
@@ -6,13 +6,14 @@ struct oneArrayStyle{N,B} <: AbstractGPUArrayStyle{N} end
 oneArrayStyle{M,B}(::Val{N}) where {N,M,B} = oneArrayStyle{N,B}()
 
 # identify the broadcast style of a (wrapped) oneArray
-BroadcastStyle(::Type{<:oneArray{T,N,B}}) where {T,N,B} = oneArrayStyle{N,B}()
-BroadcastStyle(W::Type{<:oneWrappedArray{T,N}}) where {T,N} =
+Broadcast.BroadcastStyle(::Type{<:oneArray{T, N, B}}) where {T, N, B} = oneArrayStyle{N, B}()
+Broadcast.BroadcastStyle(W::Type{<:oneWrappedArray{T, N}}) where {T, N} =
     oneArrayStyle{N, buftype(Adapt.unwrap_type(W))}()
 
 # when we are dealing with different buffer styles, we cannot know
 # which one is better, so use shared memory
-BroadcastStyle(::oneArrayStyle{N, B1},
+Broadcast.BroadcastStyle(
+    ::oneArrayStyle{N, B1},
                ::oneArrayStyle{N, B2}) where {N,B1,B2} =
     oneArrayStyle{N, oneL0.SharedBuffer}()
 
