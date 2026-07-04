e6a2fbf8886d58ca70d824bb7a16ed2b01bb1638
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2026-07-04 16:35:57 -0300

    Skip cpu-only tests

--- test/kernelabstractions.jl
+++ test/kernelabstractions.jl
@@ -5,5 +5,7 @@ skip_tests=Set([
     "sparse",
     "Convert", # Need to opt out of i128
     "Random",
+    "CPU synchronization",
+    "fallback test: callable types"
 ])
 Testsuite.testsuite(oneAPIBackend, "oneAPI", oneAPI, oneArray, oneDeviceArray; skip_tests)
